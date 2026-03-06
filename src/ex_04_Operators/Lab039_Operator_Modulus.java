34d3d08a8aebf950eeee80eebceec3c19521fcf4
Pooja Khatua <simi.khatua@gmail.com>
2026-03-06 12:42:06 +0530

    JavaBasics1

---
+++ src/ex_04_Operators/Lab039_Operator_Modulus.java
@@ -0,0 +1,17 @@
+package ex_04_Operators;
+
+public class Lab039_Operator_Modulus {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        System.out.println(a%b);  // modulus%
+        // 10 | 20 | 2 - Quotient
+        // | 20 |
+        // -------
+        //  0 - Remainder
+        // ---------
+        System.out.println(13%7);
+
+
+    }
+}
